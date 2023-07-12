e45120a93c102c96b945671975d52faf5e1cdc52
PRAVEENMPR <121223505+praveen7036@users.noreply.github.com>
2023-07-12 13:37:09 +0530

    Add files via upload

--- email.js
+++ email.js
@@ -2,15 +2,17 @@ function validateform(){
     var name=document.myform.name.value;
     var password=document.myform.name.value;
     if(name==null || name==""){
-        alert("name cannot null praveen");
+        alert("name cannot null ");
         return false;
-    }if(password.length<6)
+    }
+    if(password.length<6)
     {
-
-    alert("password atleast 6 digit ");
-    return false;
-
-    alert("welcome");
+        alert("password atleast 6 digit😎😎😎😎 ");
+        return false;
+    // alert("w");
+    }
+    else{
+        alert("THANK😍😍😍😍😍😍😍 AND LOVE U");
     }
 }
 
